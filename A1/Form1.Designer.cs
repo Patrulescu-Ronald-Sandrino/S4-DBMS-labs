76b53b240e41c70e7580a96931f82789e4f30f8c
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2022-03-26 17:15:00 +0200

    change button2's name

--- A1/Form1.Designer.cs
+++ A1/Form1.Designer.cs
@@ -48,7 +48,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(148, 64);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Update DB";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
