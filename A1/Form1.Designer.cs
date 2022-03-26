0e32232194b5be7be584199c522ee69d663586c7
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2022-03-26 17:26:40 +0200

    add second DataGridView + change positions & resize

--- A1/Form1.Designer.cs
+++ A1/Form1.Designer.cs
@@ -34,14 +34,16 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.coffeeShopDBDataSet = new A1.CoffeeShopDBDataSet();
             this.coffeeShopDBDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.coffeeShopDBDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.coffeeShopDBDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(621, 67);
+            this.button1.Location = new System.Drawing.Point(629, 48);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(131, 53);
             this.button1.TabIndex = 0;
@@ -51,9 +53,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(612, 247);
+            this.button2.Location = new System.Drawing.Point(621, 247);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(148, 64);
+            this.button2.Size = new System.Drawing.Size(139, 64);
             this.button2.TabIndex = 1;
             this.button2.Text = "Update DB";
             this.button2.UseVisualStyleBackColor = true;
@@ -65,7 +67,7 @@
             this.dataGridView1.DataSource = this.coffeeShopDBDataSetBindingSource;
             this.dataGridView1.Location = new System.Drawing.Point(42, 48);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(354, 142);
+            this.dataGridView1.Size = new System.Drawing.Size(431, 172);
             this.dataGridView1.TabIndex = 2;
             // 
             // coffeeShopDBDataSet
@@ -78,11 +80,20 @@
             this.coffeeShopDBDataSetBindingSource.DataSource = this.coffeeShopDBDataSet;
             this.coffeeShopDBDataSetBindingSource.Position = 0;
             // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(42, 247);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(431, 191);
+            this.dataGridView2.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -91,6 +102,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.coffeeShopDBDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.coffeeShopDBDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -102,6 +114,7 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.BindingSource coffeeShopDBDataSetBindingSource;
         private CoffeeShopDBDataSet coffeeShopDBDataSet;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
 
