2be669772728ca53c34ca7da3ff0add139bd8b4b
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2022-04-10 02:41:19 +0300

    WindowsFormsTestApp add bututton handlers

--- WindowsFormsTestApp/Form1.cs
+++ WindowsFormsTestApp/Form1.cs
@@ -16,5 +16,15 @@ namespace WindowsFormsTestApp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("clicked button 1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("clicked button 2");
+        }
     }
 }
