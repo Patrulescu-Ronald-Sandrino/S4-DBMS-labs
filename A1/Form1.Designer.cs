276c76c08a939c7347a13602f333308f9516dd10
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2022-05-15 12:08:17 +0300

    idk changes on A1 and A2

--- A1/Form1.Designer.cs
+++ A1/Form1.Designer.cs
@@ -63,8 +63,8 @@
             // 
             // dataGridView1
             // 
-            this.dataGridView1.AutoGenerateColumns = true;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            // this.dataGridView1.AutoGenerateColumns = true;
+            // this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.DataSource = this.coffeeShopDBDataSetBindingSource;
             this.dataGridView1.Location = new System.Drawing.Point(42, 48);
             this.dataGridView1.Name = "dataGridView1";
@@ -83,8 +83,8 @@
             // 
             // dataGridView2
             // 
-            this.dataGridView2.AutoGenerateColumns = true;
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            // this.dataGridView2.AutoGenerateColumns = true;
+            // this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView2.DataSource = this.coffeeShopDBDataSetBindingSource;
             this.dataGridView2.Location = new System.Drawing.Point(42, 247);
             this.dataGridView2.Name = "dataGridView2";
